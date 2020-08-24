2ec5adfc31e61e664d2a791c31089915001541d1
PK <piyakan.ka@rd.go.th>
2020-08-24 15:31:35 +0700

    initial commit

---
+++ src/atm/Account.java
@@ -0,0 +1,59 @@
+package atm;
+
+public class Account {
+    //1.field
+    private int id;
+    private String name;
+    private String type;
+    private double balance;
+
+    //2.constructor
+    public Account(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Account(int id, String name, String type, double balance) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    //3.method
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    //get & set
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
